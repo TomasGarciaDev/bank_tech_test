6b33e946163985e5a9d2a37eb430893d7797de5a
Tomas Garcia <tomasgarciadev@gmail.com>
2022-01-31 13:46:45 +0100

    testing account with emty balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,14 @@
 class Account
+  attr_reader :account
+  def initliaze
+    @account = []
+  end
 
+  def print_statement
+   
+    header = "date || credit || debit || balance "
+    statement << header
+
+    statement
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,4 +4,16 @@ describe 'Account' do
   it 'should be able to create and instance of Account' do
     expect(Account.new).to be_instance_of(Account)
   end
+
+  it 'should return an empty bank statement when first initliazed' do
+    my_account = Account.new
+    expect(my_account.account).to be_nil
+  end
+
+  # it 'should allow the client to make deposits with a date' do
+  #   my_account = Account.new
+  #   my_account.deposit(500, "10-01-2023")
+    
+  #   expect(my_account.statement).to eq(statement)
+  # end
 end
