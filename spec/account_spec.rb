a0e6b453ed3d389abcdcbbf93c89108f583d9bbf
Tomas Garcia <tomasgarciadev@gmail.com>
2022-01-31 15:08:25 +0100

    user can do 2 deposits

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -15,4 +15,11 @@ describe 'Account' do
     my_account.deposit(1000, "10/01/2023")
     expect(my_account.account).to eq(["10/01/2023 || 1000.00 || || 1000.00"])
   end
+
+  it 'should allow the client to make deposits with a date' do
+    my_account = Account.new
+    my_account.deposit(1000, "10/01/2023")
+    my_account.deposit(2000, "13/01/2023")
+    expect(my_account.account).to eq(["10/01/2023 || 1000.00 || || 1000.00", "13/01/2023 || 2000.00 || || 3000.00"])
+  end
 end
