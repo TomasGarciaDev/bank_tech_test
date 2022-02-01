09cbbbd6fced6ea51a7dbc2da3cbb6eb7d633cec
Tomas Garcia <tomasgarciadev@gmail.com>
2022-02-01 15:53:19 +0100

    Automated date for withdraw and deposit feature

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,54 +1,62 @@
 require 'account'
 
 describe 'Account' do
+  before do
+    @my_account = Account.new
+  end
+
   it 'should be able to create and instance of Account' do
-    expect(Account.new).to be_instance_of(Account)
+    expect(@my_account).to be_instance_of(Account)
   end
 
   it 'should return an empty bank statement when first initliazed' do
-    my_account = Account.new
-    expect(my_account.account).to be_empty
+    expect(@my_account.account).to be_empty
   end
 end
 
 describe '#deposit' do
+  before do
+    @my_account = Account.new
+  end
+
   it 'should allow the client to make deposits with a date' do
-    my_account = Account.new
-    my_account.deposit(1000, '10/01/2023')
-    expect(my_account.account).to eq(['10/01/2023 || 1000.00 || || 1000.00'])
+    date = Time.now.strftime("%d/%m/%Y")
+    @my_account.deposit(1000)
+    expect(@my_account.account).to eq(["#{date} || 1000.00 || || 1000.00"])
   end
 
   it 'should allow the client to make a deposits with a date' do
-    my_account = Account.new
-    my_account.deposit(1000, '10/01/2023')
-    my_account.deposit(2000, '13/01/2023')
-    expect(my_account.account).to eq(['10/01/2023 || 1000.00 || || 1000.00', '13/01/2023 || 2000.00 || || 3000.00'])
+    date = Time.now.strftime("%d/%m/%Y")
+    @my_account.deposit(1000)
+    @my_account.deposit(2000)
+    expect(@my_account.account).to eq(["#{date} || 1000.00 || || 1000.00", "#{date} || 2000.00 || || 3000.00"])
   end
 end
 
 describe '#withdrawl' do
+  before do
+    @my_account = Account.new
+  end
+
   it 'should allow the client to make a withdrawal with a date' do
-    my_account = Account.new
-    my_account.deposit(1000, '10/01/2023')
-    my_account.withdrawal(500, '14/01/2023')
-    expect(my_account.account).to eq(['10/01/2023 || 1000.00 || || 1000.00', '14/01/2023 || || 500.00 || 500.00'])
+    date = Time.now.strftime("%d/%m/%Y")
+    @my_account.deposit(1000)
+    @my_account.withdrawal(500)
+    expect(@my_account.account).to eq(["#{date} || 1000.00 || || 1000.00", "#{date} || || 500.00 || 500.00"])
   end
 
   it 'should not allow the client to make a withdrawal with a date when is no funds' do
-    my_account = Account.new
-    expect { my_account.withdrawal(500, '14/01/2023') }.to raise_error 'Sorry, not enough funds'
+    expect { @my_account.withdrawal(500) }.to raise_error 'Sorry, not enough funds'
   end
 end
 
 describe '#statement' do
   it 'should allow the client to print a statement' do
+    date = Time.now.strftime("%d/%m/%Y")
     my_account = Account.new
-    my_account.deposit(1000, '10/01/2023')
-    my_account.deposit(2000, '13/01/2023')
-    my_account.withdrawal(500, '14/01/2023')
-    expect(my_account.statement).to include('date || credit || debit || balance')
-    expect(my_account.statement).to include('10/01/2023 || 1000.00 || || 1000.00')
-    expect(my_account.statement).to include('13/01/2023 || 2000.00 || || 3000.00')
-    expect(my_account.statement).to include('14/01/2023 || || 500.00 || 2500.00')
+    my_account.deposit(1000)
+    my_account.deposit(2000)
+    my_account.withdrawal(500)
+    expect(my_account.statement).to eq(["#{date} || || 500.00 || 2500.00","#{date} || 2000.00 || || 3000.00", "#{date} || 1000.00 || || 1000.00"])
   end
 end
