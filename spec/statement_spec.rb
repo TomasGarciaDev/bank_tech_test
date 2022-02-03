de0da0055b16420cf2c20a29674a6e04267480b1
Tomas Garcia <tomasgarciadev@gmail.com>
2022-02-03 10:51:12 +0100

    Statement class rspec double updated to isntance_double

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -7,7 +7,7 @@ describe Statement do
     date = Time.now.strftime('%d/%m/%Y')
     account = ['03/02/2022 || 1000.00 || || 1000.00', '03/02/2022 || 2000.00 || || 3000.00',
                '03/02/2022 || || 500.00 || 2500.00']
-    my_account = double('Account', statement: Statement.new(account))
+    my_account = instance_double('Account', statement: Statement.new(account))
     # my_account.deposit(1000)
     # my_account.deposit(2000)
     # my_account.withdrawal(500)
